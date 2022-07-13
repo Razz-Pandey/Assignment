7ef780dd70ec160ada7b3fb666e56c7e1cf04ff0
Razz-Pandey <109227498+Razz-Pandey@users.noreply.github.com>
2022-07-13 20:28:53 +0530

    Create Ques11.c

---
+++ ASSG1/Ques11.c
@@ -0,0 +1,9 @@
+es (11 sloc)  252 Bytes
+#include<stdio.h>
+int main(){
+     int HH,MM;
+    printf("Enter time in HH:MM");
+    scanf("%d:%d",&HH,&MM);
+    printf("%d Hour and %d Minutes",HH,MM);
+    return 0;
+}
