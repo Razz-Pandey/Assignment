a4e27a210070a807640ea381ba12ea06228dd60c
Razz-Pandey <109227498+Razz-Pandey@users.noreply.github.com>
2022-07-13 20:26:34 +0530

    Create Ques4.c

---
+++ ASSG1/Ques4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float R,A;
+    printf("Enter the Radius of Circle");
+    scanf("%f",&R);
+    printf("Area of circle is %.4f having the radius %.4f",3.14*R*R,R);
+    return 0;
+}
